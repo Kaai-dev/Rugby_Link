328d9e0d193331f2d1ade603a398fec6c4fb9f7a
Kaai <jacobus@euca.co.za>
2023-10-11 19:32:08 +0200

    Minor fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '192.168.0.135', user: 'deploy', roles: %w{app db web}
+server 'ssh.rugbylink.net', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
