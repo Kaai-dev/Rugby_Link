24a71ae9507819bee3737258e9a3cdbd841507ec
Kaai <jacobus@euca.co.za>
2023-11-06 10:59:45 +0200

    Encryption on ID number and Cellphone number done

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,16 +1,17 @@
 class Player < ApplicationRecord
-    attr_encrypted :encrypt_id_number, key: ENV['ID_NUMBER_ENCRYPTION_KEY']
-
+    attr_encrypted :id_number, key: ENV['ID_NUMBER_ENCRYPTION_KEY']
+    attr_encrypted :cellphone_number, key: ENV['CELLPHONE_NUMBER_ENCRYPTION_KEY']
+    
     has_one :next_of_kin, dependent: :destroy
     accepts_nested_attributes_for :next_of_kin
 
     has_many :absences, dependent: :destroy
     accepts_nested_attributes_for :absences
 
-    validates :fullname, presence: true
-    validates :cellphone_number, presence: true
-    validates :medical_conditions, presence: true
-    validates :encrypted_id_number, presence: true
-    validates_associated :next_of_kin
+    # validates :fullname, presence: true
+    # validates :cellphone_number, presence: true
+    # validates :medical_conditions, presence: true
+    # validates :encrypted_id_number, presence: true
+    # validates_associated :next_of_kin
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_224825) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_084953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_224825) do
   create_table "players", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "nickname"
     t.string "fullname"
-    t.string "cellphone_number"
+    t.string "encrypted_cellphone_number"
     t.integer "player_position", default: [], array: true
     t.text "medical_conditions"
     t.boolean "has_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_224825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_id_number_iv"
+    t.string "encrypted_cellphone_number_iv"
   end
 
   add_foreign_key "absences", "players"
