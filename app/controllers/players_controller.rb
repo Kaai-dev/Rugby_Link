72a73800d111a994da2316fc225e9ab1484073d2
Kaai <jacobus@euca.co.za>
2023-12-07 22:40:16 +0200

    Added modal, Added helper WIP, Working on purge(Active storage)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -55,6 +55,14 @@ class PlayersController < ApplicationController
     redirect_to players_path, notice: 'Player was successfully deleted.'
   end
 
+  def delete_profile_pic
+    @player = Player.find(params[:id])
+    @player.profile_pic.purge
+
+    redirect_to player_path(@player), notice: 'Profile picture was successfully deleted.'
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_player
