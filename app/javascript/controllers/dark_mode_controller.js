d97ffd4e83b49c0b99e3e7cb3bdc80c3d7470576
goosemeister <jacobus.jgoosen@gmail.com>
2023-12-13 11:05:11 +0200

    Testing light mode

---
+++ app/javascript/controllers/dark_mode_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dark-mode"
+export default class extends Controller {
+  connect() {
+    console.log("connected")
+  }
+}
