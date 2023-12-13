29b9d506efce3e2544b8fc4a4ad28b20795fadc3
goosemeister <jacobus.jgoosen@gmail.com>
2023-12-13 13:36:55 +0200

    Darkmode WIP

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -2,7 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dark-mode"
 export default class extends Controller {
+  static targets = ["switchDarkLight"]
   connect() {
-    console.log("connected")
+
+  }
+
+  switch(event){
+    
+    if (this.switchDarkLightTarget.checked) {
+      document.documentElement.setAttribute("data-bs-theme", "light");
+      const body = document.querySelector("body");
+      body.style.backgroundColor = "#b8b8b8";
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+      const body = document.querySelector("body");
+      body.style.backgroundColor = "#505050";
+    }
   }
 }
+
+// Bug when switching to another page on while light mode is on
