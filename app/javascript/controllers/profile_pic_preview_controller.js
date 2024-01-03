db6a7198d034b9cc077b482c4f8ca07b1fb2a3e0
goosemeister <jacobus.jgoosen@gmail.com>
2024-01-03 13:29:55 +0200

    Finished profile pic save and remove, edited positions show, added back and cancel button

--- app/javascript/controllers/profile_pic_preview_controller.js
+++ app/javascript/controllers/profile_pic_preview_controller.js
@@ -2,27 +2,47 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-pic-preview"
 export default class extends Controller {
-    static targets = [ "preview", "existing", "previewMain", "existingMain" ]
+    static targets = [ "preview", "existing", "previewMain", "existingMain", "fileField", "remove", "save" ]
 
 
   
   connect() {
   }
 
-    preview(event) {
-      const profile_pic = event.target.files[0];
-      const reader = new FileReader();
+  
+  preview(event) {
+    const profile_pic = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = (e) => {
+      this.previewTarget.src = e.target.result;
+      this.previewMainTarget.src = e.target.result;
+      this.previewTarget.style.display = "inline-block";
+      this.existingTarget.style.display = "none";
+      this.previewMainTarget.style.display = "inline-block";
+      this.existingMainTarget.style.display = "none";
+      console.log(this.existingTarget)
+    };
+    reader.readAsDataURL(profile_pic);
+      
+    this.removeTarget.style.display = "inline-block";
+    this.saveTarget.style.display = "inline-block";
+ 
 
-      reader.onload = (e) => {
-        this.previewTarget.src = e.target.result;
-        this.previewMainTarget.src = e.target.result;
-        this.previewTarget.style.display = "inline-block";
-        this.existingTarget.style.display = "none";
-        this.previewMainTarget.style.display = "inline-block";
-        this.existingMainTarget.style.display = "none";
-        console.log(this.existingTarget)
-      };
-      reader.readAsDataURL(profile_pic);
+  }
+    
+    removePreview(event) {
+      this.fileFieldTarget.value = "";
+      this.removeTarget.style.display = "none";
+      this.previewMainTarget.style.display = "none";
+      this.previewTarget.style.display = "none";
+      this.existingTarget.style.display = "inline-block";
+      this.existingMainTarget.style.display = "inline-block";
+      this.saveTarget.style.display = "none";
+
+
+
     }
+
 
 }
