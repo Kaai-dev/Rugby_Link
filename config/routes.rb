61a847e91d061ae3d6c632a9189d99898449e13e
goosemeister <jacobus.jgoosen@gmail.com>
2024-02-01 09:56:09 +0200

    Fixed rails miniprofiler not wanting to deploy(routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,52 @@
 Rails.application.routes.draw do
-  mount RailsMiniProfiler::Engine => '/rails_mini_profiler'
+  mount RailsMiniProfiler::Engine => '/rails_mini_profiler' if defined?(RailsMiniProfiler)
   resources :teams
   resources :coaches
   resources :user_settings
 
   devise_for :users
 
-  root "players#index"
-
+  root 'players#index'
 
   resources :users do
     collection do
-      get "flash_back_to_index"
+      get 'flash_back_to_index'
     end
 
     member do
-      get "flash_cancel_edit"
-      get "delete_user_profile_pic"
+      get 'flash_cancel_edit'
+      get 'delete_user_profile_pic'
     end
   end
 
   resources :coaches do
     collection do
-      get "flash_back_to_index"
+      get 'flash_back_to_index'
     end
 
     member do
-      get "flash_cancel_edit"
-      get "delete_coach_profile_pic"
+      get 'flash_cancel_edit'
+      get 'delete_coach_profile_pic'
     end
   end
 
   resources :players do
     member do
-      get "remove_copy_of_id"
+      get 'remove_copy_of_id'
     end
     collection do
-      get "flash_back_to_index"
+      get 'flash_back_to_index'
     end
 
     member do
-      get "flash_cancel_edit"
-      get "delete_player_profile_pic"
+      get 'flash_cancel_edit'
+      get 'delete_player_profile_pic'
     end
   end
 
   resources :user_settings do
     member do
-      patch "dark_mode_route"
+      patch 'dark_mode_route'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
